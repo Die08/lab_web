de75060eacfe3322c9ba360580ea63d4ffdecb50
Die08 <03diegocarta@gmail.com>
2025-04-10 17:24:10 +0200

    add app

---
+++ app/data/books.py
@@ -0,0 +1,10 @@
+#operazione che prende i dati da un database
+#per ora lo simuliamo
+
+from models.book import Book
+
+books={
+    0: Book(id=0,title="l 0",author="a 0",review=1),
+    1: Book(id=1,title="l 1",author="a 1",review=2),
+    2: Book(id=2,title="l 2",author="a 2",review=5)
+}
---
+++ app/models/book.py
@@ -0,0 +1,14 @@
+
+from pydantic import BaseModel, Field
+from typing import Annotated
+
+class Book(BaseModel):
+    id: int #tipo di dato che accetta
+    title: str
+    author: str
+    #review: int | None=None #rendiamo opzionale la recensione, di default è none
+    review: Annotated[int|None, Field(ge=1, le=5)]=None
+
+book=Book(id=1, title="titolo", author="<NAME>",review=1)
+
+print(book)
